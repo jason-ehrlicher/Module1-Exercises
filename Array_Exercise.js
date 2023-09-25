e22b98177df7dcf0c73416b7327b5aadeadfb8d3
jason-ehrlicher <jehrlich@uno.edu>
2023-09-25 14:56:22 -0500

    Adding the array exercise

---
+++ Array_Exercise.js
@@ -0,0 +1,8 @@
+const numberArray = [5, 10, 15, 20, 25]; 
+
+console.log("Original Array:", numberArray); 
+
+numberArray[1] = 55; 
+numberArray[4] = 100; 
+
+console.log("New Array", numberArray); 
