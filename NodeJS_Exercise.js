60ebcf0d7aa030f6760ce04394257193633ac64d
jason-ehrlicher <jehrlich@uno.edu>
2023-09-25 14:41:50 -0500

    Adding the Node Js Exercise

---
+++ NodeJS_Exercise.js
@@ -0,0 +1,30 @@
+function sum(A, B){
+    console.log(A+B)
+}
+
+function subtract(A, B){
+    console.log(A - B)
+}
+
+function multiply (A, B){
+    console.log(A * B)
+}
+
+function divide (A, B){
+    console.log(A / B)
+}
+
+const A = 20
+const B = 5
+
+function sayHello(name){
+    console.log('Hello ' + name)
+}
+
+const name = "Jason"
+
+console.log(sum(A, B));
+console.log(subtract(A, B));
+console.log(multiply(A, B));
+console.log(divide(A, B)); 
+console.log(sayHello(name));
