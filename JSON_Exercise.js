165afa689f4cfd06ac1c8543d0305485bca50af8
jason-ehrlicher <jehrlich@uno.edu>
2023-09-25 14:57:59 -0500

    Adding the JSON exercise

---
+++ JSON_Exercise.js
@@ -0,0 +1,12 @@
+let book = {
+    "title": "The Giver",
+    "description":
+    "'The Giver' is a dystopian novel where Jonas uncovers forgotten emotions, challenging conformity.",
+    "author": "Lois Lowry",
+    "number_of_pages": 240,
+}
+
+console.log(book.title)
+console.log(book.description)
+console.log(book.author)
+console.log(book.number_of_pages)
